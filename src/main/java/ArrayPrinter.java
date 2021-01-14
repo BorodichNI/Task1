566857fe7424d5b0f739563492f2c02c4099d432
BorodichNI <73989351+BorodichNI@users.noreply.github.com>
2021-01-14 20:00:32 +0300

    Update ArrayPrinter.java

--- src/main/java/ArrayPrinter.java
+++ src/main/java/ArrayPrinter.java
@@ -1,6 +1,6 @@
 public class ArrayPrinter {
 
-    public void printer(int[][] array) {
+    public void print(int[][] array) {
         for (int d = 0; d < array.length; d++) {
             for (int s = 0; s < array.length; s++) {
                 System.out.print(array[d][s] + "\t");
