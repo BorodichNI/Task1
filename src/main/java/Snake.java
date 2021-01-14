5e51f8911ef048bea16bd4a1dc3ed3262832fe61
BorodichNI <73989351+BorodichNI@users.noreply.github.com>
2021-01-14 19:01:55 +0300

    Update Snake.java

--- src/main/java/Snake.java
+++ src/main/java/Snake.java
@@ -1,39 +1,39 @@
 public class Snake implements Aggregate {
 
     @Override
-    public int[][] aggregate(int n) {
-        int[][] array = new int[n][n];
+    public int[][] aggregate(int size) {
+        int[][] array = new int[size][size];
 
         int row = 0;
         int col = 0;
         int step = 0;
-        int i = 0;
+        int index = 0;
 
-        for (int z = 0; i < n * n - 1; z++) {
+        for (int z = 0; index < size * size - 1; z++) {
 
-            for (int j = 0; j < n - step; j++) {
-                array[row][col++] = ++i;
+            for (int j = 0; j < size - step; j++) {
+                array[row][col++] = ++index;
             }
             col--;
             ++step;
 
-            for (int k = 0; k < n - step; k++) {
-                array[++row][col] = ++i;
+            for (int k = 0; k < size - step; k++) {
+                array[++row][col] = ++index;
             }
 
-            for (int l = 0; l < n - step; l++) {
-                array[row][--col] = ++i;
+            for (int l = 0; l < size - step; l++) {
+                array[row][--col] = ++index;
             }
             ++step;
 
-            for (int m = 0; m < n - step; m++) {
-                array[--row][col] = ++i;
+            for (int m = 0; m < size - step; m++) {
+                array[--row][col] = ++index;
             }
             col++;
 
-            if (n % 2 != 0) {
-                for (int b = 0; b < n - step; b++) {
-                    array[row][col] = ++i;
+            if (size % 2 != 0) {
+                for (int b = 0; b < size - step; b++) {
+                    array[row][col] = ++index;
                 }
             }
         }
